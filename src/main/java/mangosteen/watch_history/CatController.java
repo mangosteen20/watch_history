a6509c0ce90bf34f237b7b68a57accc3b14e1176
shuowww <shuowww@gmail.com>
2019-01-07 23:58:07 -0800

    update top3 category functionality

--- src/main/java/mangosteen/watch_history/CatController.java
+++ src/main/java/mangosteen/watch_history/CatController.java
@@ -2,7 +2,10 @@ package mangosteen.watch_history;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +32,42 @@ public class CatController {
 
     private final CategoryResourceAssembler cat_assem;
 
+    private List<Category> cat_top3;
+
     CatController(CatRepository repository, CategoryResourceAssembler assembler) {
         this.cat_repo = repository;
         this.cat_assem = assembler;
+        this.cat_top3 = findTop3();
+    }
+
+    private List<Category> findTop3() {
+        List<Category> top3 = new ArrayList<Category>();
+
+        if (cat_repo == null) {
+            return top3;
+        }
+
+        List<Category> categories = cat_repo.findAll();
+        PriorityQueue<Category> cat_max = new PriorityQueue<Category>(new Comparator<Category>() {
+            @Override
+            public int compare(Category cat1, Category cat2) {
+                return cat2.getTime() - cat1.getTime();
+            }
+        });
+        for (Category cat: categories) {
+            cat_max.add(cat);
+        }
+
+        int i = 0;
+        while (!cat_max.isEmpty() && i <= 2) {
+            top3.add(cat_max.poll());
+            i++;
+        }
+        while(i <= 2) {
+            top3.add(new Category());
+            i++;
+        }
+        return top3;
     }
 
     @GetMapping("/categories")
@@ -42,6 +78,17 @@ public class CatController {
         return new Resources<>(categories,
                 linkTo(methodOn(CatController.class).all()).withSelfRel());
     }
+
+    @GetMapping("/top3")
+    Resources<Resource<Category>> top3(){
+        cat_top3 = findTop3();
+        List<Resource<Category>> categories = cat_top3.stream()
+                .map(cat_assem::toResource)
+                .collect(Collectors.toList());
+        return new Resources<>(categories,
+                linkTo(methodOn(CatController.class).top3()).withSelfRel());
+    }
+
 
     @PostMapping("/categories")
     ResponseEntity<?> newCategory(@RequestBody Category newCategory) throws URISyntaxException {
@@ -66,7 +113,7 @@ public class CatController {
         Category updatedCategory = cat_repo.findById(id)
                 .map(category -> {
                     category.setName(newCategory.getName());
-                    category.setTime(newCategory.getTime());
+                    category.setTime(category.getTime()+newCategory.getTime());
                     return cat_repo.save(category);
                 })
                 .orElseGet(() -> {
